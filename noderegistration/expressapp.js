81e28401f7f2752bfb225d1bd4d8c823a671c739
ptomer40 <ptomer40@yahoo.com>
2025-04-29 10:09:39 +0530

    database connection

--- noderegistration/expressapp.js
+++ noderegistration/expressapp.js
@@ -1,24 +1,39 @@
 const express=require('express');
 const cors=require('cors');
 const fs=require('fs').promises;
+const student=require('./model/student')
 const app=express();
+const dbconn=require('./database/dbConn')
 const port=3002;
+dbconn();  //database connection calling
 app.use(express.json()); // middleware
 app.use(cors()); //allow cross origing request
-app.post('/login',(req,res)=>{
+app.post('/login',async(req,res)=>{
     try{
         console.log("Hello")
-    const {name,email} =req.body;
-    console.log(name+email);
+    const {password,email} =req.body;
+    console.log(password+email);
+    const status=await student.findOne({email:email});
+    if(status){
+        if(status.password==password){
+            res.status(200).json({msg:"Successfully login"});
+        }
+    }else{
+        res.status(200).json({msg:"Email is not registered"}); 
+    }
 
 //res.send("<h2>Welcome to express server</h2>");
-res.status(200).json({msg:"Successfully login"});
+
     }catch(err){
         res.status(500).json({msg:err});
     }
 })
 
-app.post('/register',(req,res)=>{
+app.post('/register',async(req,res)=>{
+const sregdata=req.body;
+//console.log(sregdata);
+await student.create(sregdata);
+
    // res.send("<h2>Welcome to express server post method</h2>");
    res.status(200).json({msg:"Successfully registered"});
 })
